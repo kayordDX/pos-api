6619f040b177251645636b510ada169fd6589d8c
Jaco Kok <kokjaco2@gmail.com>
2023-11-08 22:04:49 +0200

    Program

--- src/Kayord.POS/Program.cs
+++ src/Kayord.POS/Program.cs
@@ -3,6 +3,8 @@ global using FastEndpoints;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
 
+// builder.Services.AddEntityFrameworkNpgsql()
+
 var app = builder.Build();
 app.UseFastEndpoints();
 app.Run();
