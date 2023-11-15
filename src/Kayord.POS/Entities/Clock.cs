09a4f811373573accde6f471e2a7a754d6ea2d98
Jaco Kok <kokjaco2@gmail.com>
2023-11-15 22:25:32 +0200

    Added clock

---
+++ src/Kayord.POS/Entities/Clock.cs
@@ -0,0 +1,12 @@
+namespace Kayord.Pos.Entities;
+
+public class Clock
+{
+    public int Id { get; set; }
+    public int StaffId { get; set; }
+    public Staff Staff { get; set; } = default!;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int SalesPeriodId { get; set; }
+    public SalesPeriod SalesPeriod { get; set; } = default!;
+}
