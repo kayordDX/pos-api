39162642aa691c0b1f6ad075e4072688ac283e2e
Stefan Jonck <stonck08@gmail.com>
2023-11-28 23:15:00 +0200

    Removed 7

--- src/Kayord.Pos/Features/Table/GetBooked/EndPoint.cs
+++ src/Kayord.Pos/Features/Table/GetBooked/EndPoint.cs
@@ -28,7 +28,7 @@ namespace Kayord.Pos.Features.Table.GetMyBooked
             {
                 //current bookings
             var results = await _dbContext.TableBooking
-                .Where(booking => booking.Table.Section.OutletId == req.OutletId && booking.StaffId == 7 &&
+                .Where(booking => booking.Table.Section.OutletId == req.OutletId && booking.StaffId == _cu.Id &&
                                   _dbContext.TableCashUp.All(cashUp => cashUp.TableBookingId != booking.Id))
                 .Where(x => x.Table.Section.OutletId == req.OutletId)
                 .ProjectToDto()
