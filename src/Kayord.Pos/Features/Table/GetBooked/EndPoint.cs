8b2c5c41881569de62296baccc7b798b64f1b0d6
Stefan Jonck <stonck08@gmail.com>
2024-01-23 23:26:12 +0200

    Fixed UserId on CurrentUserService

--- src/Kayord.Pos/Features/Table/GetBooked/EndPoint.cs
+++ src/Kayord.Pos/Features/Table/GetBooked/EndPoint.cs
@@ -28,7 +28,7 @@ namespace Kayord.Pos.Features.Table.GetMyBooked
             {
                 //current bookings
             var results = await _dbContext.TableBooking
-                .Where(booking => booking.Table.Section.OutletId == req.OutletId && booking.StaffId == _cu.Id &&
+                .Where(booking => booking.Table.Section.OutletId == req.OutletId && booking.StaffId.ToString() == _cu.UserId &&
                                   _dbContext.TableCashUp.All(cashUp => cashUp.TableBookingId != booking.Id))
                 .Where(x => x.Table.Section.OutletId == req.OutletId)
                 .ProjectToDto()
@@ -38,7 +38,7 @@ namespace Kayord.Pos.Features.Table.GetMyBooked
             }
             else{
                var results = await _dbContext.TableBooking
-                .Where(booking => booking.Table.Section.OutletId == req.OutletId && booking.StaffId != _cu.Id &&
+                .Where(booking => booking.Table.Section.OutletId == req.OutletId && booking.StaffId.ToString() != _cu.UserId &&
                                   _dbContext.TableCashUp.All(cashUp => cashUp.TableBookingId != booking.Id))
                 .Where(x => x.Table.Section.OutletId == req.OutletId)
                 .ProjectToDto()
--- src/Kayord.Pos/Features/User/GetRoles/EndPoint.cs
+++ src/Kayord.Pos/Features/User/GetRoles/EndPoint.cs
@@ -1,41 +1,41 @@
-// using Kayord.Pos.Data;
-// using Kayord.Pos.Services;
-// using Microsoft.EntityFrameworkCore;
+using Kayord.Pos.Data;
+using Kayord.Pos.Services;
+using Microsoft.EntityFrameworkCore;
 
-// namespace Kayord.Pos.Features.User.GetRoles
-// {
-//     public class Endpoint : Endpoint<Request, List<Response>>
-//     {
-//         private readonly AppDbContext _dbContext;
-//         private readonly CurrentUserService _cu;
+namespace Kayord.Pos.Features.User.GetRoles
+{
+    public class Endpoint : Endpoint<Request, List<string>>
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly CurrentUserService _cu;
 
-//         public Endpoint(AppDbContext dbContext, CurrentUserService cu)
-//         {
-//             _dbContext = dbContext;
-//             _cu = cu;
-//         }
+        public Endpoint(AppDbContext dbContext, CurrentUserService cu)
+        {
+            _dbContext = dbContext;
+            _cu = cu;
+        }
 
-//         public override void Configure()
-//         {
-//             Get("/user/getroles");
-//             AllowAnonymous();
-//         }   
+        public override void Configure()
+        {
+            Get("/user/getroles");
+            AllowAnonymous();
+        }   
  
-//         public override async Task HandleAsync(Request req, CancellationToken ct)  
-//         {
+        public override async Task HandleAsync(Request req, CancellationToken ct)  
+        {
          
-//               var userRoles = await _dbContext.UserRole
-//                 .Include(ur => ur.Role)
-//                 .Where(ur => ur.UserID == _cu.Id)
-//                 .Select(ur => ur.Role.Name)
-//                 .ToListAsync();
-//             if (userRoles == null)
-//              {
-//                 await SendNotFoundAsync();
-//                 return;
-//              }
-//                 await SendAsync(userRoles);
-//                 return;
-//         }
-//     }
-//     }
+              var userRoles = await _dbContext.UserRole
+                .Include(ur => ur.Role)
+                .Where(ur => ur.UserId == _cu.UserId)
+                .Select(ur => ur.Role.Name)
+                .ToListAsync();
+            if (userRoles == null)
+             {
+                await SendNotFoundAsync();
+                return;
+             }
+                await SendAsync(userRoles);
+                return;
+        }
+    }
+    }
