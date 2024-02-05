a2c32e855f595145358d8b7e719028ffa816cf8d
Stefan Jonck <stonck08@gmail.com>
2024-02-05 23:15:13 +0200

    Assign Outlet

---
+++ src/Kayord.Pos/Features/User/AssignOutlet/Endpoint.cs
@@ -0,0 +1,45 @@
+using Kayord.Pos.Data;
+using Kayord.Pos.Data.Migrations;
+using Kayord.Pos.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kayord.Pos.Features.User.AssignOutlet;
+
+public class Endpoint : Endpoint<Request, Entities.UserOutlet>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly CurrentUserService _cu;
+
+    public Endpoint(AppDbContext dbContext, CurrentUserService cu)
+    {
+        _dbContext = dbContext;
+        _cu = cu;
+    }
+
+    public override void Configure()
+    {
+        Post("/user/assignOutlet");
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+
+        var outlet = await _dbContext.UserOutlet.FirstOrDefaultAsync(x => x.UserId == _cu.UserId && x.OutletId == req.OutletId);
+        if (outlet == null)
+        {
+            outlet = new()
+            {
+                OutletId = req.OutletId,
+                UserId = _cu.UserId,
+                isCurrent = true
+            };
+            await _dbContext.AddAsync(outlet);
+        }
+        else
+        {
+            outlet.isCurrent = true;
+        }
+        await _dbContext.SaveChangesAsync();
+        await SendAsync(outlet);
+    }
+}
---
+++ src/Kayord.Pos/Features/User/AssignOutlet/Request.cs
@@ -0,0 +1,8 @@
+using FluentValidation;
+namespace Kayord.Pos.Features.User.AssignOutlet
+{
+    public class Request
+    {
+        public int OutletId { get; set; }
+    }
+}
