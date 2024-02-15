6a47ec242db6ed82b38b97f7a4fef8ffeeabe884
Jaco Kok <kokjaco2@gmail.com>
2024-02-16 00:14:07 +0200

    Add OrderItem back

--- src/Kayord.Pos/Entities/OrderItemExtra.cs
+++ src/Kayord.Pos/Entities/OrderItemExtra.cs
@@ -3,6 +3,8 @@ public class OrderItemExtra
 {
     public int OrderItemExtraId { get; set; }
     public int OrderItemId { get; set; }
+    public OrderItem OrderItem { get; set; } = default!;
     public int ExtraId { get; set; }
-    // public Extra Extra { get; set; } = default!;
+    public Extra Extra { get; set; } = default!;
+
 }
