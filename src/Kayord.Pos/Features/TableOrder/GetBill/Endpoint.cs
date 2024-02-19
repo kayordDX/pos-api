0442c98a41736f95211c5cf34cb029c4ecdc7a6d
Stefan Jonck <stonck08@gmail.com>
2024-02-19 21:38:26 +0200

    Bill Order Maping

---
+++ src/Kayord.Pos/Features/TableOrder/GetBill/Endpoint.cs
@@ -0,0 +1,57 @@
+using Kayord.Pos.Data;
+using Kayord.Pos.Entities;
+using Kayord.Pos.Services;
+using Kayord.Pos.DTO;
+
+using Microsoft.EntityFrameworkCore;
+using SqlKata;
+using Kayord.Pos.Common.Wrapper;
+
+namespace Kayord.Pos.Features.TableOrder.GetBill;
+
+public class Endpoint : Endpoint<Request, Response>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly CurrentUserService _cu;
+
+    public Endpoint(AppDbContext dbContext, CurrentUserService cu)
+    {
+        _dbContext = dbContext;
+        _cu = cu;
+    }
+
+    public override void Configure()
+    {
+        Get("/order/getBill");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        Response response = new();
+        response.Total = 0;
+        var tableBooking = await _dbContext.TableBooking.FirstOrDefaultAsync(x => x.Id == req.TableBookingId);
+        if (tableBooking == null)
+            await SendNotFoundAsync();
+        response.OrderItems = await _dbContext.OrderItem
+        .Where(x => x.TableBookingId == req.TableBookingId)
+        .ProjectToDto()
+        .ToListAsync();
+
+        foreach (BillOrderItemDTO item in response.OrderItems)
+        {
+            response.Total += item.MenuItem.Price;
+            if (item.Options != null)
+                foreach (OptionDTO option in item.Options)
+                {
+                    response.Total += option.Price;
+                }
+            if (item.Extras != null)
+                foreach (ExtraDTO extra in item.Extras)
+                {
+                    response.Total += extra.Price;
+                }
+        }
+        await SendAsync(response);
+    }
+}
