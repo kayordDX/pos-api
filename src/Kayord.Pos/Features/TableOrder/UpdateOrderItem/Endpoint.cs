d4c4548c57d9022cee8367ba64caeed90bbd99f8
Stefan Jonck <stonck08@gmail.com>
2024-02-19 23:50:27 +0200

    Remove to Post

--- src/Kayord.Pos/Features/TableOrder/UpdateOrderItem/Endpoint.cs
+++ src/Kayord.Pos/Features/TableOrder/UpdateOrderItem/Endpoint.cs
@@ -14,7 +14,7 @@ namespace Kayord.Pos.Features.TableOrder.UpdateOrderItem
 
         public override void Configure()
         {
-            Delete("/order/updateOrderItem");
+            Post("/order/updateOrderItem");
         }
 
         public override async Task HandleAsync(Request req, CancellationToken ct)
@@ -25,7 +25,7 @@ namespace Kayord.Pos.Features.TableOrder.UpdateOrderItem
                 entity.OrderItemStatusId = req.OrderItemStatusId;
                 if (req.isComplete)
                     entity.OrderCompleted = DateTime.Now;
-                _dbContext.Remove(entity);
+
                 await _dbContext.SaveChangesAsync();
                 await SendAsync(new Response() { IsSuccess = true });
 
