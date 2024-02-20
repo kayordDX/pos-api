8fe5008f3072ed7ccceedb46805e430274d5f55f
Jaco Kok <kokjaco2@gmail.com>
2024-02-20 23:16:48 +0200

    Response type fix

--- src/Kayord.Pos/Features/TableOrder/Kitchen/Response.cs
+++ src/Kayord.Pos/Features/TableOrder/Kitchen/Response.cs
@@ -4,7 +4,7 @@ namespace Kayord.Pos.Features.Kitchen.GetOrders;
 
 public class Response
 {
-    public int TableBookingId {get;set;}
-    public string TableName {get;set;}
+    public int TableBookingId { get; set; }
+    public string TableName { get; set; } = string.Empty;
     public List<BillOrderItemDTO> OrderItems { get; set; } = new List<BillOrderItemDTO>();
 }
