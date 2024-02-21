5af21f4445615acfc828e169dae49c3dc7ce83a9
Jaco Kok <kokjaco2@gmail.com>
2024-02-21 09:40:20 +0200

    DTO Fix

--- src/Kayord.Pos/Features/TableOrder/Kitchen/TableBookingDTO.cs
+++ src/Kayord.Pos/Features/TableOrder/Kitchen/TableBookingDTO.cs
@@ -1,3 +1,5 @@
+using Kayord.Pos.DTO;
+
 namespace Kayord.Pos.Features.TableOrder.Kitchen;
 public class TableBookingDTO
 {
@@ -5,5 +7,7 @@ public class TableBookingDTO
     public int TableId { get; set; } = default!;
     public TableDTO Table { get; set; } = default!;
     public List<OrderItemDTO>? OrderItems { get; set; }
-
+    public string BookingName { get; set; } = string.Empty;
+    public DateTime BookingDate { get; set; } = DateTime.UtcNow;
+    public UserDTO User { get; set; } = default!;
 }
