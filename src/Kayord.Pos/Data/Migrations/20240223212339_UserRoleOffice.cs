d648d5f8e8a72dc7c270085966a2752fd5e3ef10
Stefan Jonck <stonck08@gmail.com>
2024-02-23 23:23:48 +0200

    Migration

---
+++ src/Kayord.Pos/Data/Migrations/20240223212339_UserRoleOffice.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kayord.Pos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRoleOffice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isBackOffice",
+                table: "UserRole",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isFrontLine",
+                table: "UserRole",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isBackOffice",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "isFrontLine",
+                table: "UserRole");
+        }
+    }
+}
