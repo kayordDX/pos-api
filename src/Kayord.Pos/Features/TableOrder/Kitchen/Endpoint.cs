cad03eb4e3edd0ede46a006a841a3fe296fe2b89
Stefan Jonck <stonck08@gmail.com>
2024-02-23 23:37:47 +0200

    Extra Check remove

--- src/Kayord.Pos/Features/TableOrder/Kitchen/Endpoint.cs
+++ src/Kayord.Pos/Features/TableOrder/Kitchen/Endpoint.cs
@@ -52,21 +52,19 @@ public class Endpoint : EndpointWithoutRequest<Response>
         if (role!.isBackOffice)
             result.ForEach(dto =>
             {
-                if (role!.isBackOffice)
-                    dto.OrderItems = dto.OrderItems!
-                    .Where(oi => statusIds.Contains(oi.OrderItemStatusId) &&
-                        divisionIds.Contains(oi.MenuItem.DivisionId))
-                    .ToList();
+                dto.OrderItems = dto.OrderItems!
+                .Where(oi => statusIds.Contains(oi.OrderItemStatusId) &&
+                    divisionIds.Contains(oi.MenuItem.DivisionId))
+                .ToList();
             });
 
         if (role!.isFrontLine)
             result.Where(x => x.User.UserId == _cu.UserId).ToList().ForEach(dto =>
             {
-                if (role!.isBackOffice)
-                    dto.OrderItems = dto.OrderItems!
-                    .Where(oi => statusIds.Contains(oi.OrderItemStatusId) &&
-                        divisionIds.Contains(oi.MenuItem.DivisionId))
-                    .ToList();
+                dto.OrderItems = dto.OrderItems!
+                .Where(oi => statusIds.Contains(oi.OrderItemStatusId) &&
+                    divisionIds.Contains(oi.MenuItem.DivisionId))
+                .ToList();
             });
         result = result.Where(x => x.OrderItems!.Any()).ToList();
 
