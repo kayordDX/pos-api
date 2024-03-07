ce5ee59c28b71c3b0fcda42a27c91d7e0e621cbf
Stefan Jonck <stonck08@gmail.com>
2024-03-07 21:56:42 +0200

    BugFix on TableOrderUpdate

--- src/Kayord.Pos/Features/TableOrder/UpdateTableOrder/Endpoint.cs
+++ src/Kayord.Pos/Features/TableOrder/UpdateTableOrder/Endpoint.cs
@@ -1,4 +1,5 @@
 using Kayord.Pos.Data;
+using Kayord.Pos.DTO;
 using Kayord.Pos.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,30 +17,38 @@ namespace Kayord.Pos.Features.TableOrder.UpdateTableOrder
         public override void Configure()
         {
             Post("/order/updateTableOrder");
+            AllowAnonymous();
         }
 
         public override async Task HandleAsync(Request req, CancellationToken ct)
         {
             var updateableStatus = await _dbContext.OrderItemStatus.Where(x => x.isBillable == true && x.isComplete == false).Select(rd => rd.OrderItemStatusId).ToListAsync();
             var ois = await _dbContext.OrderItemStatus.FirstOrDefaultAsync(x => x.OrderItemStatusId == req.OrderItemStatusId);
+            Entities.Table table = new();
+            bool notify = true;
             if (updateableStatus != null && ois != null)
             {
-                List<OrderItem>? entities = await _dbContext.OrderItem.Where(x => updateableStatus.Contains(x.OrderItemStatusId) && x.TableBookingId == req.TableBookingId).ToListAsync();
-                foreach (OrderItem i in entities)
+                List<OrderItemDTO>? entities = await _dbContext.OrderItem.Where(x => updateableStatus.Contains(x.OrderItemStatusId) && x.TableBookingId == req.TableBookingId).ProjectToDto().ToListAsync();
+                foreach (OrderItemDTO i in entities)
                 {
-                    i.OrderItemStatusId = req.OrderItemStatusId;
-                    i.OrderUpdated = DateTime.UtcNow;
-
+                    OrderItem oi = await _dbContext.OrderItem.FindAsync(i.OrderItemId) ?? new();
+                    oi.OrderItemStatusId = req.OrderItemStatusId;
+                    oi.OrderUpdated = DateTime.UtcNow;
+                    if (table.TableId != i.TableBooking.TableId)
+                        table = await _dbContext.Table.FindAsync(i.TableBooking.TableId) ?? new();
                     if (ois.isComplete)
                         i.OrderCompleted = DateTime.Now;
-                    if (ois.Notify)
+                    if (ois.Notify && table.TableId == i.TableBooking.TableId && notify)
+                    {
                         _dbContext.Add(new UserNotification()
                         {
                             UserId = i.TableBooking.UserId,
-                            Notification = i.TableBooking.Table.Name + "- All Orders - " + ois.Status,
+                            Notification = table.Name + "- All Orders - " + ois.Status,
                             DateSent = DateTime.Now,
                             DateExpires = DateTime.Now.AddMinutes(30)
                         });
+                        notify = false;
+                    }
                 }
 
                 await _dbContext.SaveChangesAsync();
