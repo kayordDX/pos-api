f89bf4037de67ff23a7d881c577acd339594db9f
Stefan Jonck <stonck08@gmail.com>
2024-04-25 20:23:02 +0200

    Migration and Status Log

---
+++ src/Kayord.Pos/Data/Migrations/20240425182150_OrderItemStatusLog.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Kayord.Pos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderItemStatusLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderItemStatusLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderItemId = table.Column<int>(type: "integer", nullable: false),
+                    OrderItemStatusId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItemStatusLog", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItemStatusLog");
+        }
+    }
+}
---
+++ src/Kayord.Pos/Entities/OrderItemStatusLog.cs
@@ -0,0 +1,6 @@
+public class OrderItemStatusLog
+{
+    public int Id { get; set; }
+    public int OrderItemId { get; set; }
+    public int OrderItemStatusId { get; set; }
+}
