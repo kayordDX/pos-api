4ac7df7d3a95cf84878885de0853eba8ba4aea7f
Jaco Kok <kokjaco2@gmail.com>
2024-05-02 16:23:32 +0200

    Cleanup on some items.

--- src/Kayord.Pos/Common/Extensions/AuthExtensions.cs
+++ src/Kayord.Pos/Common/Extensions/AuthExtensions.cs
@@ -35,27 +35,6 @@ public static class AuthExtensions
                     ValidateLifetime = true
                 };
             });
-        // .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o =>
-        // {
-        //     o.Events = new JwtBearerEvents
-        //     {
-        //         OnMessageReceived = context =>
-        //         {
-        //             var accessToken = context.Request.Query["access_token"];
-        //             var path = context.HttpContext.Request.Path;
-        //             if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hub"))
-        //             {
-        //                 context.Token = accessToken;
-        //             }
-        //             return Task.CompletedTask;
-        //         }
-        //     };
-        //     o.Authority = configuration["Auth:ValidIssuer"];
-        //     o.Audience = configuration["Auth:Audience"];
-        //     o.TokenValidationParameters.ValidIssuer = configuration["Auth:ValidIssuer"];
-        //     o.MapInboundClaims = false;
-        // });
-
         services.AddAuthorization();
         return services;
     }
--- src/Kayord.Pos/Services/CurrentUserService.cs
+++ src/Kayord.Pos/Services/CurrentUserService.cs
@@ -12,7 +12,6 @@ public class CurrentUserService
     }
     public string? Expires => _httpContextAccessor.HttpContext?.User?.FindFirstValue("exp");
     public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue("user_id");
-    // TODO: Email is empty. Can we remove this?
     public string? Email => _httpContextAccessor.HttpContext?.User?.FindFirstValue("email");
     public string? Name => _httpContextAccessor.HttpContext?.User?.FindFirstValue("name");
 }
