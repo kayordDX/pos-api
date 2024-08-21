34e3f47ff54b5797f08e8f3f430bc9e1a5271ce1
Jaco Kok <kokjaco2@gmail.com>
2024-08-21 22:37:26 +0200

    Extra fields for front end

--- src/Kayord.Pos/Features/Printer/PrinterStatus.cs
+++ src/Kayord.Pos/Features/Printer/PrinterStatus.cs
@@ -1,10 +1,13 @@
 using ESCPOS_NET;
+using Humanizer;
 
 namespace Kayord.Pos.Features.Printer;
 
 public class PrinterStatus
 {
     public DateTime DateUpdated { get; set; } = DateTime.Now;
+    public bool IsOutdated => DateTime.Now - DateUpdated > TimeSpan.FromMinutes(10);
+    public string DateUpdatedFormatted => DateUpdated.Humanize();
     public PrinterConfig PrinterConfig { get; set; } = new();
     public PrinterStatusEventArgs? PrinterStatusEventArgs { get; set; } = null;
 }
