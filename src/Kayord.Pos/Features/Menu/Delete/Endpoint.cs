f7a45b1dc4c82707dcc952a225eff95b69b47000
Jaco Kok <kokjaco2@gmail.com>
2024-11-18 20:11:01 +0200

    Added redis and made delete be in path.

--- src/Kayord.Pos/Features/Menu/Delete/Endpoint.cs
+++ src/Kayord.Pos/Features/Menu/Delete/Endpoint.cs
@@ -1,24 +1,28 @@
 using Kayord.Pos.Data;
+using Kayord.Pos.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kayord.Pos.Features.Menu.Delete;
 
 public class Endpoint : Endpoint<Request>
 {
     private readonly AppDbContext _dbContext;
+    private readonly RedisClient _redisClient;
 
-    public Endpoint(AppDbContext dbContext)
+    public Endpoint(AppDbContext dbContext, RedisClient redisClient)
     {
         _dbContext = dbContext;
+        _redisClient = redisClient;
     }
 
     public override void Configure()
     {
-        Delete("/menu");
+        Delete("/menu/{id}");
     }
 
     public override async Task HandleAsync(Request req, CancellationToken ct)
     {
-        var entity = await _dbContext.Menu.FindAsync(req.Id);
+        var entity = await _dbContext.Menu.FirstOrDefaultAsync(x => x.Id == req.Id);
         if (entity == null)
         {
             await SendNotFoundAsync();
@@ -27,7 +31,7 @@ public class Endpoint : Endpoint<Request>
 
         _dbContext.Menu.Remove(entity);
         await _dbContext.SaveChangesAsync();
-
+        await Helper.ClearCacheOutlet(_dbContext, _redisClient, entity.OutletId);
         await SendNoContentAsync();
     }
 }
