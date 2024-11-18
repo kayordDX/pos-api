e0665b8486d732b20c7b2ecd461abc0218dce967
Jaco Kok <kokjaco2@gmail.com>
2024-11-18 23:04:27 +0200

    Response fix

--- src/Kayord.Pos/Features/Manager/Overview/Response.cs
+++ src/Kayord.Pos/Features/Manager/Overview/Response.cs
@@ -1,11 +1,9 @@
-using Kayord.Pos.Entities;
-
 namespace Kayord.Pos.Features.Manager.OrderView;
 
 public class Response
 {
     public int DivisionId { get; set; }
-    public Division Division { get; set; } = default!;
+    public Entities.Division Division { get; set; } = default!;
     public List<TableBookingDTO>? Tables { get; set; }
     public DateTime LastRefresh { get; set; }
     public int PendingTables { get; set; }
