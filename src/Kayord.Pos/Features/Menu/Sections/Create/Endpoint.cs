74f727755afc7e7ac96e24d6769266e77619e2dc
Stefan Jonck <stonck08@gmail.com>
2024-11-18 21:01:17 +0200

    MenuSection Add

---
+++ src/Kayord.Pos/Features/Menu/Sections/Create/Endpoint.cs
@@ -0,0 +1,51 @@
+using Kayord.Pos.Data;
+using Kayord.Pos.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Kayord.Pos.Features.Menu.Sections.Create;
+
+public class Endpoint : Endpoint<Request, Pos.Entities.MenuSection>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly RedisClient _redisClient;
+
+    public Endpoint(AppDbContext dbContext, RedisClient redisClient)
+    {
+        _dbContext = dbContext;
+        _redisClient = redisClient;
+    }
+
+    public override void Configure()
+    {
+        Post("/menuSection");
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+
+        Entities.Menu? menu = await _dbContext.Menu.FirstOrDefaultAsync(x => x.Id == req.MenuId);
+
+        if (menu != null)
+        {
+            var menuSection = new Entities.MenuSection
+            {
+                MenuId = req.MenuId,
+                Name = req.Name,
+                PositionId = req.PositionId,
+                ParentId = req.ParentId
+            };
+
+            await _dbContext.MenuSection.AddAsync(menuSection);
+            await _dbContext.SaveChangesAsync();
+            await Helper.ClearCacheOutlet(_dbContext, _redisClient, menu.OutletId);
+
+        }
+        else
+        {
+            throw new Exception("Menu not found");
+        }
+
+
+    }
+}
---
+++ src/Kayord.Pos/Features/Menu/Sections/Create/Request.cs
@@ -0,0 +1,13 @@
+
+
+namespace Kayord.Pos.Features.Menu.Sections.Create;
+
+public class Request
+{
+    public string? Name { get; set; } = string.Empty;
+    public int MenuId { get; set; }
+    public int? ParentId { get; set; }
+    public int? PositionId { get; set; }
+
+}
+
