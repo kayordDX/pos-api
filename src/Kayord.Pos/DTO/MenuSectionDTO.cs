688f32a79e91e34224b8f8f21ada500e1b17a043
Jaco Kok <kokjaco2@gmail.com>
2024-11-18 21:14:17 +0200

    Fixed nullable name bug

--- src/Kayord.Pos/DTO/MenuSectionDTO.cs
+++ src/Kayord.Pos/DTO/MenuSectionDTO.cs
@@ -2,7 +2,7 @@ namespace Kayord.Pos.DTO;
 public class MenuSectionDTO
 {
     public int MenuSectionId { get; set; }
-    public string? Name { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public int MenuId { get; set; }
     public int? ParentId { get; set; }
     public MenuSectionParentDTO? Parent { get; set; }
