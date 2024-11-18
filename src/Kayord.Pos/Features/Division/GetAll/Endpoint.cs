14ab4c8a50e1493debeedc7ec5ce681468904710
Stefan Jonck <stonck08@gmail.com>
2024-11-18 22:59:44 +0200

    Division Get All

---
+++ src/Kayord.Pos/Features/Division/GetAll/Endpoint.cs
@@ -0,0 +1,26 @@
+using Kayord.Pos.Data;
+using Kayord.Pos.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kayord.Pos.Features.Division.GetAll;
+
+public class Endpoint : EndpointWithoutRequest<List<Pos.Entities.Division>>
+{
+    private readonly AppDbContext _dbContext;
+
+    public Endpoint(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public override void Configure()
+    {
+        Get("/division");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var results = await _dbContext.Division.ToListAsync();
+        await SendAsync(results);
+    }
+}
