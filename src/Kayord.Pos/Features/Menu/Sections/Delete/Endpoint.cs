f86c74857af32a0a06e86d65c7b4fc647aff2d12
Stefan Jonck <stonck08@gmail.com>
2024-11-18 21:10:23 +0200

    Temp Delete

--- src/Kayord.Pos/Features/Menu/Sections/Delete/Endpoint.cs
+++ src/Kayord.Pos/Features/Menu/Sections/Delete/Endpoint.cs
@@ -24,24 +24,9 @@ public class Endpoint : Endpoint<Request, Pos.Entities.MenuSection>
     public override async Task HandleAsync(Request req, CancellationToken ct)
     {
 
-        Entities.Menu? menu = await _dbContext.MenuSection.FirstOrDefaultAsync(x => x.MenuSectionId == req.Id);
+        Entities.MenuSection? menu = await _dbContext.MenuSection.FirstOrDefaultAsync(x => x.MenuSectionId == req.Id);
 
-        if (menu != null)
-        {
-            var menuSection = new Entities.MenuSection
-            {
-                MenuId = req.MenuId,
-                Name = req.Name,
-                PositionId = req.PositionId,
-                ParentId = req.ParentId
-            };
 
-            await _dbContext.MenuSection.AddAsync(menuSection);
-            await _dbContext.SaveChangesAsync();
-            await Helper.ClearCacheOutlet(_dbContext, _redisClient, menu.OutletId);
-
-        }
-        else
         {
             throw new Exception("Menu not found");
         }
