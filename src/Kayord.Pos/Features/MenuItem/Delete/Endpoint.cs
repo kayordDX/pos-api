66d99a55e2cd88d0a6774218df6d5abb42f097e6
Stefan Jonck <stonck08@gmail.com>
2024-11-18 22:15:46 +0200

    Menu Item Delete

---
+++ src/Kayord.Pos/Features/MenuItem/Delete/Endpoint.cs
@@ -0,0 +1,48 @@
+using Kayord.Pos.Data;
+using Kayord.Pos.Entities;
+using Kayord.Pos.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Kayord.Pos.Features.MenuItem.Delete;
+
+public class Endpoint : Endpoint<Request, Pos.Entities.MenuSection>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly RedisClient _redisClient;
+
+    public Endpoint(AppDbContext dbContext, RedisClient redisClient)
+    {
+        _dbContext = dbContext;
+        _redisClient = redisClient;
+    }
+
+    public override void Configure()
+    {
+        Delete("/menuItem/{id}");
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+
+        Entities.MenuItem? menuItem = await _dbContext.MenuItem
+        .Include(x => x.MenuSection)
+        .ThenInclude(x => x.Menu)
+        .FirstOrDefaultAsync(x => x.MenuItemId == req.Id);
+
+        if (menuItem != null)
+        {
+            _dbContext.MenuItem.Remove(menuItem);
+            await _dbContext.SaveChangesAsync();
+            await Helper.ClearCacheOutlet(_dbContext, _redisClient, menuItem.MenuSection.Menu!.OutletId);
+        }
+
+        else
+        {
+            throw new Exception("Sorry, the princess is in another castle.");
+        }
+    }
+
+}
+
+
