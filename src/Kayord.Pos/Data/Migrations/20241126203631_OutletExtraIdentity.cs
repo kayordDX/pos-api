ca0e82ad9adc45065ef4f4d1ac9c8a91b6ae8fb0
Stefan Jonck <stonck08@gmail.com>
2024-11-26 22:38:15 +0200

    OutletExtra Id auto

---
+++ src/Kayord.Pos/Data/Migrations/20241126203631_OutletExtraIdentity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kayord.Pos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OutletExtraIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                ALTER TABLE "OutletExtraGroup" ALTER "Id" RESTART 100;
+            """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
