7d611a10e6421aa415dc05006603c8a67ff86396
Stefan Jonck <stonck08@gmail.com>
2024-11-26 21:08:33 +0200

    Add Extra

---
+++ src/Kayord.Pos/Features/Extra/Create/Endpoint.cs
@@ -0,0 +1,54 @@
+using Kayord.Pos.Data;
+using Kayord.Pos.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Kayord.Pos.Features.Extra.Create;
+
+public class Endpoint : Endpoint<Request, Pos.Entities.MenuItem>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly RedisClient _redisClient;
+
+    public Endpoint(AppDbContext dbContext, RedisClient redisClient)
+    {
+        _dbContext = dbContext;
+        _redisClient = redisClient;
+    }
+
+    public override void Configure()
+    {
+        Post("/extra");
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+
+        Entities.ExtraGroup? extraGroup = await _dbContext.ExtraGroup.FirstOrDefaultAsync(x => x.ExtraGroupId == req.ExtraGroupId);
+
+        if (extraGroup != null)
+        {
+
+            Entities.Extra extra = new()
+            {
+                Name = req.Name,
+                PositionId = req.PositionId,
+                Price = req.Price,
+                ExtraGroupId = req.ExtraGroupId,
+                OutletId = req.OutletId
+            };
+
+            await _dbContext.Extra.AddAsync(extra);
+            await _dbContext.SaveChangesAsync();
+
+            // await Helper.ClearCacheOutlet(_dbContext, _redisClient, req.OutletId);
+
+        }
+        else
+        {
+            throw new Exception("Extra Group not found");
+        }
+
+
+    }
+}
---
+++ src/Kayord.Pos/Features/Extra/Create/Request.cs
@@ -0,0 +1,12 @@
+namespace Kayord.Pos.Features.Extra.Create;
+
+public class Request
+{
+    public string Name { get; set; } = string.Empty;
+    public int PositionId { get; set; }
+    public decimal Price { get; set; }
+    public int ExtraGroupId { get; set; }
+    public int OutletId { get; set; }
+
+}
+
