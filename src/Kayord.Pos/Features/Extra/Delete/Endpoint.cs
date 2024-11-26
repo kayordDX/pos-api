ae645534612f37fcb06130caae24300e6ba48db8
Stefan Jonck <stonck08@gmail.com>
2024-11-26 21:44:01 +0200

    Delete The Extra

---
+++ src/Kayord.Pos/Features/Extra/Delete/Endpoint.cs
@@ -0,0 +1,43 @@
+using Kayord.Pos.Data;
+using Kayord.Pos.Entities;
+using Kayord.Pos.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Kayord.Pos.Features.Extra.Delete;
+
+public class Endpoint : Endpoint<Request>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly RedisClient _redisClient;
+
+    public Endpoint(AppDbContext dbContext, RedisClient redisClient)
+    {
+        _dbContext = dbContext;
+        _redisClient = redisClient;
+    }
+
+    public override void Configure()
+    {
+        Delete("/extra/{id}");
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+
+        Entities.Extra? extra = await _dbContext.Extra.FindAsync(req.Id);
+
+        if (extra != null)
+        {
+            _dbContext.Extra.Remove(extra);
+            await _dbContext.SaveChangesAsync();
+        }
+        else
+        {
+            throw new Exception("Extra Not Found");
+        }
+    }
+
+}
+
+
