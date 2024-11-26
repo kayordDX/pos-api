d492de5c9c7df5e8e08693c98ea1c685d7f06e6a
Stefan Jonck <stonck08@gmail.com>
2024-11-26 22:25:55 +0200

    Create Extra Group

---
+++ src/Kayord.Pos/Features/Extra/GroupCreate/Endpoint.cs
@@ -0,0 +1,56 @@
+using Kayord.Pos.Data;
+using Kayord.Pos.Data.Migrations;
+using Kayord.Pos.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Kayord.Pos.Features.Extra.GroupCreate;
+
+public class Endpoint : Endpoint<Request>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly RedisClient _redisClient;
+    private readonly CurrentUserService _user;
+
+    public Endpoint(AppDbContext dbContext, RedisClient redisClient, CurrentUserService user)
+    {
+        _dbContext = dbContext;
+        _redisClient = redisClient;
+        _user = user;
+    }
+
+    public override void Configure()
+    {
+        Post("/extraGroup");
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        var outletId = await Helper.GetUserOutlet(_dbContext, _user.UserId ?? "");
+
+        Entities.ExtraGroup? extraGroup = new()
+        {
+            Name = req.Name,
+            OutletId = outletId
+        };
+
+        await _dbContext.ExtraGroup.AddAsync(extraGroup);
+
+        if (req.isGlobal)
+        {
+            Entities.OutletExtraGroup outletExtraGroup = new()
+            {
+                OutletId = extraGroup.OutletId,
+                ExtraGroupId = extraGroup.ExtraGroupId
+            };
+            await _dbContext.OutletExtraGroup.AddAsync(outletExtraGroup);
+        }
+        await _dbContext.SaveChangesAsync();
+        await SendNoContentAsync();
+        // await Helper.ClearCacheOutlet(_dbContext, _redisClient, req.OutletId);
+
+    }
+
+
+}
+
---
+++ src/Kayord.Pos/Features/Extra/GroupCreate/Request.cs
@@ -0,0 +1,10 @@
+namespace Kayord.Pos.Features.Extra.GroupCreate;
+
+public class Request
+{
+    public string Name { get; set; } = string.Empty;
+    public bool isGlobal { get; set; }
+
+
+}
+
