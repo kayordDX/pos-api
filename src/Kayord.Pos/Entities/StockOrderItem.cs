881eec712fdb6df53b3d5f5423b65fbdd7180a53
Jaco Kok <kokjaco2@gmail.com>
2025-02-09 16:12:44 +0200

    Status And Amount to StockOrderItem

--- src/Kayord.Pos/Entities/StockOrderItem.cs
+++ src/Kayord.Pos/Entities/StockOrderItem.cs
@@ -6,6 +6,9 @@ public class StockOrderItem : AuditableEntity
     public StockOrder StockOrder { get; set; } = default!;
     public int StockId { get; set; }
     public Stock Stock { get; set; } = default!;
+    public decimal OrderAmount { get; set; }
+    public int StockOrderItemStatusId { get; set; }
+    public StockOrderItemStatus StockOrderItemStatus { get; set; } = default!;
     public decimal Actual { get; set; }
     public decimal Price { get; set; }
 }
