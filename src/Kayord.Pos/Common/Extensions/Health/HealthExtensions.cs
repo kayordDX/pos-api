794c927007c0bdf4873b57d267cc5d5494189c6b
Jaco Kok <kokjaco2@gmail.com>
2025-04-06 13:44:53 +0200

    Latest packages and redis health check added.

--- src/Kayord.Pos/Common/Extensions/Health/HealthExtensions.cs
+++ src/Kayord.Pos/Common/Extensions/Health/HealthExtensions.cs
@@ -6,7 +6,10 @@ public static class HealthExtensions
 {
     public static void ConfigureHealth(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddHealthChecks().AddProcessAllocatedMemoryHealthCheck(150).AddNpgSql(configuration.GetConnectionString("DefaultConnection")!);
+        services.AddHealthChecks()
+            .AddProcessAllocatedMemoryHealthCheck(1750)
+            .AddNpgSql(configuration.GetConnectionString("DefaultConnection")!)
+            .AddRedis(configuration.GetConnectionString("Redis")!);
     }
 
     public static IApplicationBuilder UseHealth(this IApplicationBuilder app)
