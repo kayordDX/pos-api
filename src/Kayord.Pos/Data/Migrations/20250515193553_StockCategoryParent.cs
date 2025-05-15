990b984c28e43196b0b18163e6b9e6786088c0e5
Jaco Kok <kokjaco2@gmail.com>
2025-05-15 22:23:56 +0200

    Also included parentId for them sub categories

---
+++ src/Kayord.Pos/Data/Migrations/20250515193553_StockCategoryParent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kayord.Pos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StockCategoryParent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "parent_id",
+                table: "stock_category",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "parent_id",
+                table: "stock_category");
+        }
+    }
+}
--- src/Kayord.Pos/Entities/StockCategory.cs
+++ src/Kayord.Pos/Entities/StockCategory.cs
@@ -3,6 +3,7 @@ namespace Kayord.Pos.Entities;
 public class StockCategory : AuditableEntity
 {
     public int Id { get; set; }
+    public int? ParentId { get; set; }
     public int OutletId { get; set; }
     public string Name { get; set; } = string.Empty;
 }
