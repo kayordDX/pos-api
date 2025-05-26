28fe53415842db42799eebd526494ba007e77832
Jaco Kok <kokjaco2@gmail.com>
2025-05-26 21:34:09 +0200

    ParentId can now be nullable.

--- src/Kayord.Pos/Features/Stock/Category/Response.cs
+++ src/Kayord.Pos/Features/Stock/Category/Response.cs
@@ -4,7 +4,7 @@ public class Response
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public int ParentId { get; set; }
+    public int? ParentId { get; set; }
     public string ParentName { get; set; } = string.Empty;
     public int OutletId { get; set; }
     public string DisplayName { get; set; } = string.Empty;
