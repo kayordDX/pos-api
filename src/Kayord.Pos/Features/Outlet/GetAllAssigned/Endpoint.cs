986266135690613ec3ba7ca26c64d5d502c3faa9
Jaco Kok <kokjaco2@gmail.com>
2025-05-27 22:18:07 +0200

    New Switch outlet option

--- src/Kayord.Pos/Features/Outlet/GetAllAssigned/Endpoint.cs
+++ src/Kayord.Pos/Features/Outlet/GetAllAssigned/Endpoint.cs
@@ -1,11 +1,11 @@
 using Kayord.Pos.Data;
-using Kayord.Pos.Entities;
+using Kayord.Pos.DTO;
 using Kayord.Pos.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Kayord.Pos.Features.Outlet.GetAllAssigned;
 
-public class Endpoint : EndpointWithoutRequest<List<Pos.Entities.Outlet>>
+public class Endpoint : EndpointWithoutRequest<List<OutletDTOBasic>>
 {
     private readonly AppDbContext _dbContext;
     private readonly CurrentUserService _cu;
@@ -23,11 +23,20 @@ public class Endpoint : EndpointWithoutRequest<List<Pos.Entities.Outlet>>
 
     public override async Task HandleAsync(CancellationToken ct)
     {
-        var results = await _dbContext.UserRoleOutlet
-            .Where(x => x.UserId == _cu.UserId)
-            .Include(x => x.Outlet)
-            .Select(x => x.Outlet)
-            .ToListAsync();
+        var results = await _dbContext.Database.SqlQuery<OutletDTOBasic>($"""
+            select distinct
+                o.id, 
+                o.name, 
+                o.vat_number, 
+                o.address, 
+                o.company, 
+                o.registration
+                from outlet o
+            join user_role_outlet ur
+                on ur.outlet_id = o.id
+            where ur.user_id = {_cu.UserId}
+        """).ToListAsync(ct);
+
         await SendAsync(results);
     }
 }
