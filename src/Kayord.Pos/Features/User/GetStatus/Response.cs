b1dc8d72b6c5fa3aa090748630560e33fe727d49
Jaco Kok <kokjaco2@gmail.com>
2025-06-18 21:53:01 +0200

    Status includes divisions and optimisations

--- src/Kayord.Pos/Features/User/GetStatus/Response.cs
+++ src/Kayord.Pos/Features/User/GetStatus/Response.cs
@@ -1,16 +1,25 @@
 namespace Kayord.Pos.Features.User.GetStatus;
 
-using Kayord.Pos.Entities;
 public class Response
 {
     public int OutletId { get; set; }
     public string? OutletName { get; set; }
     public bool ClockedIn { get; set; }
     public int SalesPeriodId { get; set; }
-    public SalesPeriod? SalesPeriod { get; set; }
+    public SalesPeriodDTO? SalesPeriod { get; set; }
     public List<RoleDTO> Roles { get; set; } = new List<RoleDTO>();
+    public List<DivisionDTO> Divisions { get; set; } = new List<DivisionDTO>();
     public bool hasNotification { get; set; }
     public int StatusId { get; set; }
+}
+
+public class SalesPeriodDTO
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public int OutletId { get; set; }
 }
 
 public class RoleDTO
@@ -18,4 +27,10 @@ public class RoleDTO
     public int Id { get; set; }
     public string RoleName { get; set; } = string.Empty;
     public string RoleType { get; set; } = string.Empty;
+}
+
+public class DivisionDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
