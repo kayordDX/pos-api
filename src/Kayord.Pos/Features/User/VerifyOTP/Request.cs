02faa1008194e9cd51fd4c45d97e263700da8aa1
Jaco Kok <kokjaco2@gmail.com>
2025-06-20 23:31:02 +0200

    Validation added.

--- src/Kayord.Pos/Features/User/VerifyOTP/Request.cs
+++ src/Kayord.Pos/Features/User/VerifyOTP/Request.cs
@@ -1,6 +1,16 @@
+using FluentValidation;
+
 namespace Kayord.Pos.Features.User.VerifyOTP;
 
 public class Request
 {
     public string OTP { get; set; } = string.Empty;
+}
+
+public class Validator : Validator<Request>
+{
+    public Validator()
+    {
+        RuleFor(v => v.OTP).Length(8).WithMessage("OTP Must have length of 8");
+    }
 }
