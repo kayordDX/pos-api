b6783b8f82ddbd6643abb8bfcf2d1fb8beb05997
Jaco Kok <kokjaco2@gmail.com>
2025-07-01 22:52:29 +0200

    Returning features even when no sales period.

--- src/Kayord.Pos/Features/User/GetStatus/Endpoint.cs
+++ src/Kayord.Pos/Features/User/GetStatus/Endpoint.cs
@@ -83,6 +83,12 @@ public class Endpoint : EndpointWithoutRequest<Response>
         """).ToListAsync(ct);
         resp.Divisions = divisions;
 
+        var features = await _dbContext.OutletFeature
+            .Where(x => x.OutletId == userOutlet.OutletId)
+            .Select(x => x.Feature)
+            .ToListAsync(ct);
+        resp.Features = features;
+
         var salesPeriod = await _dbContext.SalesPeriod
             .Where(x => x.OutletId == userOutlet.OutletId && x.StartDate != null && x.EndDate == null)
             .Select(x => new SalesPeriodDTO() { Id = x.Id, Name = x.Name, EndDate = x.EndDate, StartDate = x.StartDate })
@@ -102,12 +108,6 @@ public class Endpoint : EndpointWithoutRequest<Response>
         var clockInStatus = await _dbContext.Clock
             .AnyAsync(x => x.UserId == _cu.UserId && x.OutletId == userOutlet.OutletId && x.EndDate == null);
         resp.ClockedIn = clockInStatus;
-
-        var features = await _dbContext.OutletFeature
-            .Where(x => x.OutletId == userOutlet.OutletId)
-            .Select(x => x.Feature)
-            .ToListAsync(ct);
-        resp.Features = features;
 
         // Check if user has notification. TODO: Make this more generic
         // Get all waiting items for user
