b154cb3c2522dcea8d02bc9adf46f7d64f98897d
Stefan Jonck <stonck08@gmail.com>
2025-07-08 21:50:55 +0200

    Front and Back Only

--- src/Kayord.Pos/Features/Role/Type/GetAll/Endpoint.cs
+++ src/Kayord.Pos/Features/Role/Type/GetAll/Endpoint.cs
@@ -16,10 +16,12 @@ namespace Kayord.Pos.Features.Role.Type.GetAll
         {
             Get("/role/type/{outletId}");
         }
-
+        //exclude type admin and guest
         public override async Task HandleAsync(Request req, CancellationToken ct)
         {
-            var roles = await _dbContext.RoleType.ToListAsync();
+            var roles = await _dbContext.RoleType
+            .Where(x => x.Name.ToLower() != "manager" && x.Name.ToLower() != "guest")
+            .ToListAsync();
             await SendAsync(roles);
         }
     }
