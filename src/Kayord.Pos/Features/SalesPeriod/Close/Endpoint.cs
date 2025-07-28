374393be6b3f2cea1ee8ecc465534373440fd4e4
Jaco Kok <kokjaco2@gmail.com>
2025-07-28 21:14:53 +0200

    Added check before closing sales period to make sure all orders are closed

--- src/Kayord.Pos/Features/SalesPeriod/Close/Endpoint.cs
+++ src/Kayord.Pos/Features/SalesPeriod/Close/Endpoint.cs
@@ -29,8 +29,29 @@ public class Endpoint : Endpoint<Request, Entities.SalesPeriod>
         var OpenTableCount = await _dbContext.TableBooking.Where(x => x.SalesPeriodId == req.SalesPeriodId && x.CloseDate == null).CountAsync();
         if (OpenTableCount > 0)
         {
-            throw new Exception("Cannot close sales period with open tables");
+            ValidationContext.Instance.ThrowError("Cannot close sales period with open tables");
         }
+
+        // Check if any in progress orders
+        OrderResult? inProgressOrderCount = await _dbContext.Database.SqlQuery<OrderResult>($"""
+            SELECT 
+                count(order_item_id) count  
+            FROM order_item oi
+            JOIN order_item_status ois
+                ON oi.order_item_status_id = ois.order_item_status_id
+            JOIN table_booking tb
+                ON tb.id = oi.table_booking_id
+            JOIN sales_period s
+                ON s.id = tb.sales_period_id
+            WHERE ois.is_back_office = true
+            AND s.outlet_id = {entity.OutletId}
+        """).FirstOrDefaultAsync(ct);
+
+        if (inProgressOrderCount?.Count > 0)
+        {
+            ValidationContext.Instance.ThrowError("Cannot close sales period with open orders");
+        }
+
         entity.EndDate = DateTime.Now;
 
         // Clock out all users
---
+++ src/Kayord.Pos/Features/SalesPeriod/Close/OrderResult.cs
@@ -0,0 +1,6 @@
+namespace Kayord.Pos.Features.SalesPeriod.Close;
+
+public class OrderResult
+{
+    public int Count { get; set; }
+}
