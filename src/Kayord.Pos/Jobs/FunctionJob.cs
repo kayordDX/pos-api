3503e6476b556128f7e75d7dfa9dd34636000a07
Jaco Kok <kokjaco2@gmail.com>
2025-08-07 23:30:19 +0200

    Updated Stock Threshold ticker cron

--- src/Kayord.Pos/Jobs/FunctionJob.cs
+++ src/Kayord.Pos/Jobs/FunctionJob.cs
@@ -14,19 +14,19 @@ public class FunctionJob
         _dbContext = dbContext;
     }
 
-    [TickerFunction("Stock Threshold")]
+    [TickerFunction("Stock Threshold", "0 1 * * *")]
     public async Task StockThreshold(CancellationToken ct)
     {
         await _dbContext.Database.ExecuteSqlAsync($"SELECT update_stock_threshold();", ct);
     }
 
-    [TickerFunction("Notification Log Cleanup", "0 1 * * *")]
+    [TickerFunction("Notification Log Cleanup", "8 0 * * *")]
     public async Task NotificationLogCleanup(CancellationToken ct)
     {
         await _dbContext.Database.ExecuteSqlAsync($"delete from notification_log where date_inserted < NOW() - INTERVAL '1 months';", ct);
     }
 
-    [TickerFunction("Stock Available Check All", "0 2 * * *")]
+    [TickerFunction("Stock Available Check All", "15 1 * * *")]
     public async Task StockCheckAll(CancellationToken ct)
     {
         await StockManager.StockAvailableAllCheck(_dbContext, ct);
