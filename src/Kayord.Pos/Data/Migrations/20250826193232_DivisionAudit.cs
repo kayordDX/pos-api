0d7ad678eb3c80bddb3ea3c7432821a7990ea5af
Jaco Kok <kokjaco2@gmail.com>
2025-08-26 21:33:45 +0200

    Auditing on division

---
+++ src/Kayord.Pos/Data/Migrations/20250826193232_DivisionAudit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kayord.Pos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DivisionAudit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created",
+                table: "division",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "created_by",
+                table: "division",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_modified",
+                table: "division",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "last_modified_by",
+                table: "division",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created",
+                table: "division");
+
+            migrationBuilder.DropColumn(
+                name: "created_by",
+                table: "division");
+
+            migrationBuilder.DropColumn(
+                name: "last_modified",
+                table: "division");
+
+            migrationBuilder.DropColumn(
+                name: "last_modified_by",
+                table: "division");
+        }
+    }
+}
