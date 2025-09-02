a7aaefc389a9af9a9a32038f3269bc1beaf6ef6f
Jaco Kok <kokjaco2@gmail.com>
2025-09-02 21:29:40 +0200

    Get Status not available publicly

--- src/Kayord.Pos/Features/User/GetStatus/Endpoint.cs
+++ src/Kayord.Pos/Features/User/GetStatus/Endpoint.cs
@@ -18,7 +18,6 @@ public class Endpoint : EndpointWithoutRequest<Response>
     public override void Configure()
     {
         Get("/user/getStatus");
-        AllowAnonymous();
     }
 
     public override async Task HandleAsync(CancellationToken ct)
