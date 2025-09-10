e7ea6003c5dc868278a1855ec7b6b0fc7989ae5b
Jaco Kok <kokjaco2@gmail.com>
2025-09-10 21:53:03 +0200

    Refactor SQL query in GetAllMenu endpoint for improved readability and structure

--- src/Kayord.Pos/Features/Extra/GetAllMenu/Endpoint.cs
+++ src/Kayord.Pos/Features/Extra/GetAllMenu/Endpoint.cs
@@ -27,23 +27,27 @@ public class Endpoint : Endpoint<Request, List<SpecialExtrasDTO>>
                 o.price,
                 eg.extra_group_id,
                 eg.name "extra_group_name",
+                si.division_id,
                 coalesce(min(((si.actual - oo.quantity) >= 0)::int)::bool, true) is_available
             from extra o
+            join extra_group eg
+                on eg.extra_group_id =o.extra_group_id
+            join outlet_extra_group oeg
+                on oeg.extra_group_id =o.extra_group_id                
             left join extra_stock oo
                 on oo.extra_id = o.extra_id
-            join outlet_extra_group oeg
-                    on oeg.extra_group_id =o.extra_group_id
-            join stock_item si
+            left join stock_item si
                 on si.stock_id = oo.stock_id
-            join extra_group eg
-                    on eg.extra_group_id =o.extra_group_id
-            where oeg.outlet_id = {req.OutletId} and si.division_id = {req.DivisionId}
-            group by o.extra_id,
+            where (oeg.outlet_id = {req.OutletId} and si.division_id = {req.DivisionId}) 
+                or (oeg.outlet_id = {req.OutletId} and si.division_id is null)
+            group by 
+                o.extra_id,
                 o.name,
                 o.position_id,
                 o.price,
                 eg.extra_group_id,
-                eg.name
+                eg.name,
+                si.division_id
             order by o.name;
         """).ToListAsync(ct);
         await Send.OkAsync(results);
