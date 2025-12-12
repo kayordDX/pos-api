d419fc9decc80458b01b19e8a85ffb19fd6d8c3e
Jaco Kok <kokjaco2@gmail.com>
2025-12-12 22:53:03 +0200

    Make max mem even bigger for now.

--- src/Kayord.Pos/Common/Extensions/Health/HealthExtensions.cs
+++ src/Kayord.Pos/Common/Extensions/Health/HealthExtensions.cs
@@ -7,7 +7,7 @@ public static class HealthExtensions
     public static void ConfigureHealth(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddHealthChecks()
-            .AddProcessAllocatedMemoryHealthCheck(1750)
+            .AddProcessAllocatedMemoryHealthCheck(2750)
             .AddNpgSql(configuration.GetConnectionString("DefaultConnection")!)
             .AddRedis(configuration.GetConnectionString("Redis")!);
     }
