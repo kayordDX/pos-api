1f8d6b99b153c06c9b38394814065e878619b09a
Jaco Kok <kokjaco2@gmail.com>
2026-07-14 19:58:15 +0200

    TickerQ latest. Latest packages. Latest tools.

--- src/Kayord.Pos/Data/TickerQMigrations/20260714175658_TickerQNext.Designer.cs
+++ src/Kayord.Pos/Data/TickerQMigrations/20260714175658_TickerQNext.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TickerQ.EntityFrameworkCore.DbContextFactory;
@@ -11,9 +12,11 @@ using TickerQ.EntityFrameworkCore.DbContextFactory;
 namespace Kayord.Pos.Data.TickerQMigrations
 {
     [DbContext(typeof(TickerQDbContext))]
-    partial class TickerQDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260714175658_TickerQNext")]
+    partial class TickerQNext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Kayord.Pos/Data/TickerQMigrations/20260714175658_TickerQNext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kayord.Pos.Data.TickerQMigrations
+{
+    /// <inheritdoc />
+    public partial class TickerQNext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_system_paused",
+                schema: "ticker",
+                table: "CronTickers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_system_paused",
+                schema: "ticker",
+                table: "CronTickers");
+        }
+    }
+}
